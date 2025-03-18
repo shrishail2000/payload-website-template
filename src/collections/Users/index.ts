334808756d3b61fa3ae2950892ee62d78ee79a4e
admin <admin@admins-MacBook-Pro.local>
2025-03-18 17:53:55 +0530

    first commit

---
+++ src/collections/Users/index.ts
@@ -0,0 +1,31 @@
+import type { CollectionConfig } from 'payload'
+
+import { authenticated } from '../../access/authenticated'
+
+export const Users: CollectionConfig = {
+  slug: 'users',
+  access: {
+    admin: authenticated,
+    create: authenticated,
+    delete: authenticated,
+    read: authenticated,
+    update: authenticated,
+  },
+  admin: {
+    defaultColumns: ['name', 'email'],
+    useAsTitle: 'name',
+  },
+  auth: true,
+  fields: [
+    {
+      name: 'name',
+      type: 'text',
+    },
+    {
+      name: 'role',
+      type: 'select',
+      options: ['admin', 'superadmin', 'user'],
+    }
+  ],
+  timestamps: true,
+}
